f11e1ffad9bab1fd8858a1ab96137922011fc56f
epic9toes <kr8tiveonwe@gmail.com>
2022-11-02 12:45:24 +0100

    'update'

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/epic9toes/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x2A1E66d2152dA109EB9EAf1CF95b5cEb7460c626";
